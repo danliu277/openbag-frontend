0fdd77539d3d6d3fcb404853874e98e27b7cc15e
flatironschool <danliu277@gmail.com>
2020-07-08 15:55:21 -0400

    Change navBar to fucntion

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { setUserCreator } from '../action/actionCreator'
 
-class Navbar extends Component {
-  render() {
+function Navbar(props) {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/main">Open Bag</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/main">Open Bag</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-        <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto d-flex justify-content-end">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/inventory">Inventory</Link>
-            </li>
-            {this.props.user ? <li className="nav-item">
-              <Link className="nav-link" to='#' onClick={this.props.logout}>Log Out</Link>
-            </li> :
-            <li className="nav-item">
-              <Link className="nav-link" to="/logIn">Log In</Link>
-            </li>}
-          </ul>
-        </div>
-      </nav>
+            <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto d-flex justify-content-end">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/inventory">Inventory</Link>
+                    </li>
+                    {props.user ? <li className="nav-item">
+                        <Link className="nav-link" to='#' onClick={props.logout}>Log Out</Link>
+                    </li> :
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/logIn">Log In</Link>
+                        </li>}
+                </ul>
+            </div>
+        </nav>
     )
-  }
 }
 
 
