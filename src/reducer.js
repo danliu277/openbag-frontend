d1c375c7f1a9afdae8a88b123c5578bea597d99b
flatironschool <danliu277@gmail.com>
2020-07-08 14:45:51 -0400

    Update reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,11 @@
 let initialState = {
-    top_three: [],
-    plants: [],
     user: null
 }
 
 const reducer = (prevState = initialState, action) => {
     switch (action.type) {
-        case 'SETPLANTS': 
-            return {...prevState, plants: action.plants}
         case 'SETUSER':
             return {...prevState, user: action.user}
-        case 'SETTOPTHREE':
-            return {...prevState, top_three: action.top_three}    
         default:
             return prevState
     }
