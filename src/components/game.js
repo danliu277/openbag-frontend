cdb0fa1227723e6f28d3b436e62b304afa239ca9
flatironschool <danliu277@gmail.com>
2020-07-08 16:36:22 -0400

    Add warning icon

--- src/components/game.js
+++ src/components/game.js
@@ -4,8 +4,8 @@ function Game(props) {
     const { id, name, genre, sales_price, vendor_cost, stock, threshold } = props.game;
 
     return (
-        <tr className={stock < threshold ? 'table-danger': undefined}>
-            <th scope="row">{stock < threshold && <i className="fa fa-warning" style={{color: "red"}}></i>}{id}</th>
+        <tr className={stock < threshold ? 'table-danger' : undefined}>
+            <th scope="row">{stock < threshold && <i className="fa fa-warning" style={{ color: "red" }}></i>}{id}</th>
             <td>{name}</td>
             <td>{genre}</td>
             <td>{sales_price}</td>
