010259590711941ce467e85b8207ec0bf7b39ebc
flatironschool <danliu277@gmail.com>
2020-07-09 11:28:33 -0400

    Add sales page

--- src/containers/main.js
+++ src/containers/main.js
@@ -4,6 +4,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from '../components/navBar';
 import Login from '../components/login'
 import Inventory from '../components/inventory'
+import Sales from '../components/sales'
 
 function Main(props) {
     return (
@@ -12,6 +13,7 @@ function Main(props) {
             <Switch>
                 <Route exact path="/" component={(routerProps) => <Login {...routerProps} />} />
                 <Route exact path="/inventory" component={(routerProps) => <Inventory {...routerProps} />} />
+                <Route exact path="/sales" component={(routerProps) => <Sales {...routerProps} />} />
                 <Route render={() => <Redirect to="/" />} />
             </Switch>
         </div>
