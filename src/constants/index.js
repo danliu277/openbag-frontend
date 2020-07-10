8a6464e6e5dc22c54188b5083a2fe7ffbe8b34f1
flatironschool <danliu277@gmail.com>
2020-07-10 15:47:19 -0400

    Update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://openbag-backend.herokuapp.com';
+export const API_ROOT = 'https://openbag-backend.herokuapp.com';
 
 export const HEADERS = {
     'Content-Type': 'application/json',
