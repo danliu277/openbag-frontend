82273397383fcb5159126d58a1ce2c386b55e6c6
flatironschool <danliu277@gmail.com>
2020-07-14 15:46:11 -0400

    Graph test

--- src/components/barchart.js
+++ src/components/barchart.js
@@ -1,25 +1,57 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { select } from 'd3';
+import { select, line, curveCardinal, scaleLinear, axisBottom, axisRight, axisLeft } from 'd3';
 
 const BarChart = () => {
-    const [data, setData] = useState([25, 30, 45, 60, 0])
+    const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75])
     const svgRef = useRef()
 
     useEffect(() => {
         const svg = select(svgRef.current)
+
+        const xScale = scaleLinear()
+            .domain([0, data.length - 1])
+            .range([0, 300])
+
+        const yScale = scaleLinear()
+            .domain([0, 150])
+            .range([150, 0])
+
+        const xAxis = axisBottom(xScale)
+            .ticks(data.length)
+            .tickFormat(index => index + 1)
+
+        const yAxis = axisRight(yScale)
+
         svg
-            .selectAll("circle")
-            .data(data)
-            .join("circle")
-            .attr('r', value => value)
-            .attr('cx', value => value * 2)
-            .attr('cy', value => value * 2)
-            .attr('stroke', 'red')
+            .select(".x-axis")
+            // .style("transform", "translateY(150px)")
+            .call(xAxis)
+
+        svg
+            .select(".y-axis")
+            // .style("transform", "translateX(300px)")
+            .call(yAxis)
+
+        const myLine = line()
+            .x((value, index) => index * 50)
+            .y(value => 150 - value)
+            .curve(curveCardinal)
+
+        svg
+            .selectAll("path")
+            .data([data])
+            .join("path")
+            .attr("d", value => myLine(value))
+            .attr("fill", "none")
+            .attr("stroke", "blue")
     }, [data])
 
     return (
         <div id="chart">
-            <svg ref={svgRef} />
+            <svg ref={svgRef}>
+                <g className="x-axis" />
+                <g className="y-axis" />
+            </svg>
         </div>
     )
 }
