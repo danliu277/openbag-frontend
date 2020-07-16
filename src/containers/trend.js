a356a4d8c2c842b81fe519016b050051ebe0e90e
flatironschool <danliu277@gmail.com>
2020-07-16 15:33:58 -0400

    Change chart library

--- src/containers/trend.js
+++ src/containers/trend.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import BarChart from '../components/barchart'
+import LineGraph from '../components/lineGraph'
 
 const Trends = (props) => {
     return (
         <div>
             <h3>Trends</h3>
-            <BarChart />
+            <LineGraph />
         </div>
     )
 }
